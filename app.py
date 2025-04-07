efeaad542b6e8c7dc13c817f5b969c9f00ead34a
Sidharth221 <91930899+Sidharth221@users.noreply.github.com>
2025-04-07 21:56:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ icon("🏎️")
 st.subheader("Groq Chat Streamlit App", divider="rainbow", anchor=False)
 
 client = Groq(
-    api_key=os.getenv('GROQ_API_KEY')
+    api_key=os.environ.get('GROQ_API_KEY')
     # api_key=st.secrets["GROQ_API_KEY"],
 )
 
